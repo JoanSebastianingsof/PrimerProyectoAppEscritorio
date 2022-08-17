c21cd6b3d0b9a937ecfc0b4db726814045a28a82
JoanSebastianingsof <jsgonzalezorozco@gmail.com>
2022-08-17 09:43:20 -0500

    Matrices

---
+++ ArregloBidimensional/Module1.vb
@@ -0,0 +1,43 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim matriz(0, 0) As Double
+        Dim filas As Integer
+        Dim columnas As Integer
+        Dim contador As Integer = 0
+        Do
+            Console.WriteLine("Introduzaca la cantidad de filas")
+            filas = Console.ReadLine
+        Loop While filas < 2
+        Do
+            Console.WriteLine("Introduzaca la cantidad de columnas")
+            columnas = Console.ReadLine
+        Loop While columnas < 2
+        If filas >= 2 And columnas >= 2 Then
+            ReDim matriz(filas, columnas)
+            Console.WriteLine("valor del length" & matriz.Length)
+            Console.WriteLine("valor del fila" & matriz.GetLength(0))
+            Console.WriteLine("valor del columna" & matriz.GetLength(1))
+
+        Else
+            Console.WriteLine("Valores invalidos")
+        End If
+        'Get sirve para leer la cantidad que hay en la matriz
+        For i = 0 To matriz.GetUpperBound(0) - 1
+            For j = 0 To matriz.GetUpperBound(1) - 1
+                matriz(i, j) = contador
+                contador = contador + 0.67
+            Next
+
+        Next
+        For i = 0 To matriz.GetUpperBound(0) - 1
+            For j = 0 To matriz.GetUpperBound(1) - 1
+                Console.Write(matriz(i, j) & " ")
+
+            Next
+            Console.WriteLine()
+
+        Next
+    End Sub
+
+End Module
---
+++ Arreglos/Module1.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim myArray(10) As Double
+        ' For i = 0 To 9 Step 1
+        'Variable iteradora indica la posicion del valor
+        'myArray(i) = i * 3
+        'Next
+        'For i = 0 To 9 Step 1
+        'Variable iteradora indica la posicion del valor
+        'Console.WriteLine(myArray(i))
+        'Next
+
+        'Pedir los valores al usuario y el tamaño
+        Console.WriteLine("Introduzca el tamaño del arreglo")
+        Dim Tam As Short = Console.ReadLine()
+        ReDim myArray(Tam)
+
+        For i = 0 To Tam Step 1
+            'Variable iteradora indica la posicion del valor
+            Console.WriteLine("Introduz el valor del arreglo en la posicion {0}", i)
+            myArray(i) = Console.ReadLine()
+        Next
+
+        For i = 0 To Tam Step 1
+            'Variable iteradora indica la posicion del valor
+            Console.WriteLine(myArray(i))
+        Next
+    End Sub
+
+End Module
